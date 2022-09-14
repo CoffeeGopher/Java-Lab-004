50ee18a324b87b876907cb36b640d96cd0aa59f4
Brennan <bduffy279@mycr.redwoods.edu>
2022-09-14 10:57:35 -0700

    Finished SimpleMethod3 and Radians classes

--- src/Radians.java
+++ src/Radians.java
@@ -1,26 +1,30 @@
 import java.util.Scanner;
 
 public class Radians {
-
-    public static <ADD TYPE> toRadians(<ADD TYPE> degrees) {
-        // Pur your code here
+    private static final double PI = 3.141592653589793;
+    public static double toRadians(double degrees) {
+        return (degrees * PI) / 180;
     }
 
-    public static <ADD TYPE> toDegrees(<ADD TYPE> radians) {
-        // Put your code here
+    public static double toDegrees(double radians) {
+        return (radians * 180) / PI;
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Type degrees you'd like converted to radians: ");
-        double degrees = Double.valueOf(scanner.nextLine());
+        double degrees = Double.parseDouble(scanner.nextLine());
 
         // Add System.out, your method call, and Math.toRadians call here
+        System.out.println(toRadians(degrees));
+        System.out.println(Math.toRadians(degrees));
 
         System.out.print("Type radians you'd like converted to degrees: ");
-        double radians = Double.valueOf(scanner.nextLine());
+        double radians = Double.parseDouble(scanner.nextLine());
 
         // Add System.out, your method call, and Math.toDegrees call here
+        System.out.println(toDegrees(radians));
+        System.out.println(Math.toDegrees(radians));
     }
 }
--- src/SimpleMethod3.java
+++ src/SimpleMethod3.java
@@ -9,13 +9,11 @@ public class SimpleMethod3 {
           the arguments correctly and cast upon return.
      */
     public static int square(int number) {
-        // Put your code here
-        return 0;
+        return (int) Math.pow(number, 2);
     }
 
     public static double square(double number) {
-        // Put your code here
-        return 0;
+        return Math.pow(number, 2);
     }
 
     public static void main(String[] args) {
@@ -23,13 +21,13 @@ public class SimpleMethod3 {
 
         System.out.print("What number would you like to square: ");
         /* Integers Used */
-        int input = Integer.valueOf(scanner.nextLine());
+        int input = Integer.parseInt(scanner.nextLine());
         int result = square(input);
         System.out.printf("The square of %d is %d%n", input, result);
 
         System.out.print("What number would you like to square: ");
         /* Doubles used  */
-        double input2 = Double.valueOf(scanner.nextLine());
+        double input2 = Double.parseDouble(scanner.nextLine());
         double result2 = square(input2);
         System.out.printf("The square of %f is %f%n", input2, result2);
     }
